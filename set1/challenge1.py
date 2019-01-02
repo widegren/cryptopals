659577927106830f499ec0e7c7aa937e03a48530
Emelie Widegren <emelie.widegren@klarna.com>
2019-01-02 11:39:09 +0100

    Update to python3

---
+++ set1/challenge1.py
@@ -0,0 +1,7 @@
+import codecs
+
+#set 1 challenge 1
+def hex_to_base64(str):
+	return codecs.encode(codecs.decode(str, 'hex'), 'base64').decode()
+
+print(hex_to_base64('49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d'))
