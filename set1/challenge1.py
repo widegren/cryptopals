ac82f4538046f3e9901eb0b3889a02796761f2b4
Emelie Widegren <emelie.widegren@chalmers.se>
2019-01-10 20:19:14 +0100

    Change variable name

--- set1/challenge1.py
+++ set1/challenge1.py
@@ -1,7 +1,9 @@
 import codecs
 
-#set 1 challenge 1
-def hex_to_base64(str):
-	return codecs.encode(codecs.decode(str, 'hex'), 'base64').decode()
+
+# set 1 challenge 1
+def hex_to_base64(string):
+    return codecs.encode(codecs.decode(string, 'hex'), 'base64').decode()
+
 
 print(hex_to_base64('49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d'))
